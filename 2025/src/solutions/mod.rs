29092759e430e3565bb9bd527b9c66d9b850aa20
reaganak40 <reaganak@gmail.com>
2025-12-06 21:09:30 -0500

    added template

--- 2025/src/solutions/mod.rs
+++ 2025/src/solutions/mod.rs
@@ -1,11 +1,13 @@
 use crate::solution_traits::{Solution, SolutionFactory};
 
-mod day1;
+mod day01;
+mod day02;
 
 /// Factory function to get solution for a given day
 pub fn get_solution_for_day(inputs_dir: &str, day: u8) -> Option<Box<dyn Solution>> {
     match day {
-        1 => Some(day1::Day1::init(inputs_dir, day)),
+        1 => Some(day01::Day1::init(inputs_dir, day)),
+        2 => Some(day02::Day2::init(inputs_dir, day)),
         _ => None,
     }
 }
